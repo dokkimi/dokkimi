c5d2fd9e6618af802e97b49326213fe0726f1aed
Avi <avisprince@gmail.com>
2026-06-28 02:37:16 -0400

    fix dokkimi.rb file

--- Formula/dokkimi.rb
+++ Formula/dokkimi.rb
@@ -2,7 +2,7 @@ class Dokkimi < Formula
   desc "CLI for isolated Docker environments for microservice testing"
   homepage "https://dokkimi.com"
   url "https://registry.npmjs.org/dokkimi/-/dokkimi-0.5.2.tgz"
-  sha256 "106595be8462beaf7432d6ffba560c780e083640ae1d86a0910335402179a0a6"
+  sha256 "e6dc902239aa7c10a811b1f6dcc9de20d0a2972c16abe6ef070a3bd918219428"
   license "Elastic-2.0"
 
   depends_on "node"
